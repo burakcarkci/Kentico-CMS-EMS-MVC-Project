870342c69ce8385a4bc9e4676afb3b51aa6f1cda
Burak <burakcarkci@hotmail.com>
2020-05-24 14:27:55 -0400

    Kentico CMS test application

---
+++ Kentico-MVC-Project/KenticoDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KenticoDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("8b99fa1e-4105-47f0-b00e-f1f73ca0c993")]
+
+[assembly: AssemblyProduct("KenticoDemo")]
+[assembly: AssemblyCopyright("© 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("12.0.0.0")]
+[assembly: AssemblyFileVersion("12.0.7115.13953")]
+[assembly: AssemblyInformationalVersion("12.0.29")]
